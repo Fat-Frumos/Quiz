c35b59bac5fdcc5f4229d5d6b1501492d5bd0a7e
Fat-Frumos <lokankara@i.ua>
2023-04-01 01:44:05 +0300

    changed to v 2.5.5

--- src/main/java/com/exam/question/domain/Quiz.java
+++ src/main/java/com/exam/question/domain/Quiz.java
@@ -1,11 +1,16 @@
 package com.exam.question.domain;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import java.util.List;
 
 @Data
